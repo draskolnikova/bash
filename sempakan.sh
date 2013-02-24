513222cc9d4cfb44530d92348d7c014b7fdd95f3
at- <dewanggaba@xtremenitro.org>
2013-02-24 08:15:51 +0700

    Update sempakan

--- sempakan.sh
+++ sempakan.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+owner="sempak:sempak"
+sempakan="/home/sempak/public_html/"
+strip=`hostname > /tmp/data`
+sed -i 's/.or.id//g' /tmp/data
+
   while getopts "b:d:r:h:" options
     do
       case "$options" in
         "b")
+          echo "[+] Creating backups user $OPTARG"
           /scripts/pkgaccts $OPTARG
+          file="/home/cpmove-$OPTARG.tar.gz"
+          echo "[+] Processing to dummy ..."
+          chown $owner $file
+          chmod 644 $file
+          mv $file $sempakan
+          echo "[+] Success"
+          echo "[+] Do this on your remote server -> gembalah -d $OPTARG `cat /tmp/data`"
+          rm -f /tmp/data
           ;;
         "d")
           echo "[+] Download is in progress ..."
