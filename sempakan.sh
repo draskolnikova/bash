39cebb17ae41e75946b5ac31dbaee57200547736
at- <dewanggaba@xtremenitro.org>
2013-02-24 07:54:10 +0700

    Adding bash and parameter

--- sempakan.sh
+++ sempakan.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
-# Buat DraCoola
-# Moving ke Sempak, biar semua server sempakan
-
-read bekapan
-what="/home/$bekapan"
-echo chown sempak:sempak $what;
-echo chmod 644 $what;
-echo mv $what /home/sempak/public_html;
+  while getopts "b:d:r:h:" options
+    do
+      case "$options" in
+        "b")
+          /scripts/pkgaccts $OPTARG
+          ;;
+        "d")
+          echo "[+] Download is in progress ..."
+          wget --user-aget=DraCoola/1.1 http://$3.dracoola.net/cpmove-$OPTARG.tar.gz -O /home/cpmove-$OPTARG.tar.gz
+          echo "[+] The backups sucessfully downloaded at /home/cpmove-$OPTARG.tar.gz"
+          echo -n "[?] Do you want to restore the cpmove-$OPTARG.tar.gz? [Y/N] "; read yn
+                case $yn in
+                        [Yy]* ) /scripts/restorepkg $OPTARG;;
+                        [Nn]* ) exit;;
+                        * ) echo "Default Answer (YES)"; /scripts/restorepkg $OPTARG;;
+                esac
+          echo "[+] Restoring account for $OPTARG"
+          echo "[+] Complete"
+          ;;
+        "r")
+          /scripts/restorepkg $OPTARG
+          ;;
+        *)
+          echo "DraCoola cPanel Simple Scripts"
+          echo " <dewanggaba@xtremenitro.org> "
+          echo "Usage: $0 [OPTIONS]"
+          echo " "
+          echo "Options: "
+          echo "        -b <username>           Backup packages"
+          echo "        -d <username>           Download packages from another server"
+          echo "        -help                   Display this Messages"
+          echo "        -r <username>           Restore packages"
+          ;;
+      esac
+    done
