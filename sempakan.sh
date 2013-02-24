8dc8750210e32298476b22d6e480a8b968441929
at- <dewanggaba@xtremenitro.org>
2013-02-24 19:08:48 +0700

    Add Readme

--- sempakan.sh
+++ sempakan.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# Gembalah cPanel Scripts - HOW TO
+# /-------------------------------------------------------/
+# 
+# Backup cpanel account
+# $ gembalah -b <username>
+#
+# Download and restore cpanel account from remote server
+# $ gembalah -d <username> <server>
+# 
+# Restore cpanel account
+# $ gembalah -r <username>
+# /-------------------------------------------------------/
 owner="sempak:sempak"
 sempakan="/home/sempak/public_html/"
 strip=`hostname | sed "s/.in-hell.com//"`
@@ -21,7 +33,7 @@ strip=`hostname | sed "s/.in-hell.com//"`
         "d")
           echo "[+] Download is in progress ..."
           wget -q -nv --user-agent=DraCoola/1.1 http://$3.dracoola.net/cpmove-$OPTARG.tar.gz -O /home/cpmove-$OPTARG.tar.gz
-          echo "[+] The backups sucessfully downloaded at /home/cpmove-$OPTARG.tar.gz with `ls -lah /home/cpmove-$OPTARG.tar.gz | awk '{print $5}'` Mbytes"
+          echo "[+] The backups sucessfully downloaded at /home/cpmove-$OPTARG.tar.gz with `ls -lah /home/cpmove-$OPTARG.tar.gz | awk '{print $5}'`bytes"
           echo -n "[?] Do you want to restore the cpmove-$OPTARG.tar.gz? [Y/n] "; read yn
                 case $yn in
                         [Yy]* ) echo "[+] Restoring account for $OPTARG is in progress ..."; /scripts/restorepkg $OPTARG > /dev/null; echo "[+] Restore account for $OPTARG complete!";;
