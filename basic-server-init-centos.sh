e0b2087c0733a727b6e38845601801d35f2d231d
DominiQue <dominique@dominiQue.xtremenitro.org>
2013-05-29 13:25:55 +0700

    update

--- basic-server-init-centos.sh
+++ basic-server-init-centos.sh
@@ -2,7 +2,7 @@
 # Basic Server Initialization
 # ~ting - 28/05/2013
 yum -y update
-init="wget nano yum-presto yum-plugin-security yum-plugin-protectbase"
+init="wget nano yum-presto yum-plugin-security yum-plugin-protectbase dmidecode"
 #initchk=`for i in $init; do rpm -qa | grep $i; done`
 yum -y install $init
 echo "[+] Disabling SELinux!"
@@ -10,15 +10,17 @@ sed -i 's/=enforcing/=disabled/g' /etc/sysconfig/selinux > /dev/null
 epelchk=`rpm -qa | grep epel | wc -l`
 atomicchk=`rpm -qa | grep atomic | wc -l`
 echo "[+] Adding Nginx repository .."
-wget -q -nv https://raw.github.com/at-/bash/master/nginx.repo -o /etc/yum.repos.d/nginx.repo
+wget -q -nv https://raw.github.com/at-/bash/master/nginx.repo -O /etc/yum.repos.d/nginx.repo
 echo "[+] Adding EPEL repository .."
 if [ $epelchk != 1 ];then
 	yum -y localinstall http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 fi
+echo "[+] EPEL repository already exists!"
 echo "[+] Adding Atomic repository .."
 if [ $atomicchk != 1 ];then
 	wget -q -O - http://www.atomicorp.com/installers/atomic | sh
 fi
+echo "[+] Atomic repository already exists!"
 echo "[+] Disabling atomic repository, use --enablerepo instead"
 sed -i 's/enabled = 1/enabled = 0/g' /etc/yum.repos.d/atomic.repo
 echo "[+] Cleaning & caching new repository .."
--- basic-server-software.sh
+++ basic-server-software.sh
@@ -4,7 +4,8 @@ iotop \
 sysstat \
 nginx \
 httpd-devel \
-httpd
+httpd \
+vsftpd \
 phpMyAdmin.noarch;
 atombase="mariadb-server.x86_64 \
 mariadb.x86_64 \
@@ -16,7 +17,7 @@ php-common.x86_64 \
 php-gd.x86_64 \
 php-mbstring.x86_64 \
 php-mcrypt.x86_64 \
-php-mysql.x86_64 \
+php-mysqlnd.x86_64 \
 php-pecl-imagick.x86_64 \
 php-php-gettext.x86_64 \
 php-xmlrpc.x86_64 \
