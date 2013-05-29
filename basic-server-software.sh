4e48a1e6ac17680029017b4356d3997b3e7311c5
DominiQue <dominique@dominiQue.xtremenitro.org>
2013-05-29 11:20:23 +0700

    basic software

---
+++ basic-server-software.sh
@@ -0,0 +1,13 @@
+for i in 
+bash-completion 
+htop 
+iotop 
+sysstat 
+nginx 
+mysql.x86_64 
+mysql-server.x86_64 
+mysqltuner.noarch 
+phpMyAdmin.noarch 
+mysql-devel.x86_64;
+yum -y install $i;
+done;
