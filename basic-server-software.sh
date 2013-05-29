af9f3dd89fe60721e217f348b68a5b1e89059ebb
DominiQue <dominique@dominiQue.xtremenitro.org>
2013-05-29 11:51:46 +0700

    update server software

--- basic-server-software.sh
+++ basic-server-software.sh
@@ -1,14 +1,25 @@
-for i in 
-bash-completion \
+basic="bash-completion \
 htop \
 iotop \
-sysstat \ 
+sysstat \
 nginx \
-mariadb-server.x86_64 \
+httpd-devel \
+httpd \
+phpMyAdmin.noarch;
+atombase="mariadb-server.x86_64 \
 mariadb.x86_64 \
 mariadb-libs.x86_64 \
 mariadb-devel.x86_64 \
-phpMyAdmin.noarch \
-mysql-devel.x86_64;
-yum -y install $i;
-done;
+php.x86_64 \
+php-devel.x86_64 \
+php-common.x86_64 \
+php-gd.x86_64 \
+php-mbstring.x86_64 \
+php-mcrypt.x86_64 \
+php-mysql.x86_64 \
+php-pecl-imagick.x86_64 \
+php-php-gettext.x86_64 \
+php-xmlrpc.x86_64 \
+php-xml.x86_64"
+yum -y install $basic
+yum -y install $atombase --enablerepo atomic
