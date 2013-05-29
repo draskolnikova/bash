8e02bbc7941746b6c95632d3fca46977d8ab4e0c
DominiQue <dominique@dominiQue.xtremenitro.org>
2013-05-29 13:12:10 +0700

    fpm engine

--- basic-server-software.sh
+++ basic-server-software.sh
@@ -4,7 +4,7 @@ iotop \
 sysstat \
 nginx \
 httpd-devel \
-httpd \
+httpd
 phpMyAdmin.noarch;
 atombase="mariadb-server.x86_64 \
 mariadb.x86_64 \
@@ -20,6 +20,7 @@ php-mysql.x86_64 \
 php-pecl-imagick.x86_64 \
 php-php-gettext.x86_64 \
 php-xmlrpc.x86_64 \
-php-xml.x86_64";
+php-xml.x86_64 \
+php-fpm.x86_64";
 yum -y install $basic
 yum -y install $atombase --enablerepo atomic
