1fd77661590f3481a6c1ec6d7ec6978f4f28a83a
DominiQue <dominique@dominiQue.xtremenitro.org>
2013-05-29 11:24:17 +0700

    fixing

--- basic-server-software.sh
+++ basic-server-software.sh
@@ -1,14 +1,14 @@
 for i in 
-bash-completion 
-htop 
-iotop 
-sysstat 
-nginx 
-mariadb-server.x86_64
-mariadb.x86_64
-mariadb-libs.x86_64
-mariadb-devel.x86_64
-phpMyAdmin.noarch 
+bash-completion \
+htop \
+iotop \
+sysstat \ 
+nginx \
+mariadb-server.x86_64 \
+mariadb.x86_64 \
+mariadb-libs.x86_64 \
+mariadb-devel.x86_64 \
+phpMyAdmin.noarch \
 mysql-devel.x86_64;
 yum -y install $i;
 done;
